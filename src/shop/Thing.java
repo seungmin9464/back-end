a890162e3479f060e60a330679b8578b15441757
seungmin9464 <93364004+seungmin9464@users.noreply.github.com>
2025-05-07 17:16:25 +0900

    Add files via upload

---
+++ src/shop/Thing.java
@@ -0,0 +1,15 @@
+package beginner.shop;
+
+// 프라다를 호출한 애들만 호출 당함
+
+public class Thing {
+    String name;
+
+    Thing(String modelName){
+        this.name = modelName;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
