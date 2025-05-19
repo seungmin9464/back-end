ec9882299b7281411d2fabfa61f5cad66a0325a3
seungmin9464 <93364004+seungmin9464@users.noreply.github.com>
2025-05-19 10:08:18 +0900

    shopDemo.java

--- src/beginner/shop/shopDemo.java
+++ src/beginner/shop/shopDemo.java
@@ -2,19 +2,19 @@ package beginner.shop;
 
 public class shopDemo {
     public static void main(String[] args) {
-        Brand prada = new Brand("프라다");
-        Brand gucci = new Brand("구찌");
-        Brand notThing = new Brand("");
+        Brand prada = new Brand("프라다");        // 프라다 생성자 생성
+        Brand gucci = new Brand("구찌");          // 구찌 생성자 생성
+        Brand notThing = new Brand("");           // 비어있는 생성자 생성
 
-        Brand[] brands = { prada, gucci, notThing };
-        int random = (int)(Math.random() * brands.length);
+        Brand[] brands = { prada, gucci, notThing };    // 배열 생성
+        int random = (int)(Math.random() * brands.length);    // 랜덤
 
         if( 0 == random ){
             System.out.println(prada.getName() + prada.buy("가방"));
         } else if ( 1 == random ) {
             System.out.println(gucci.getName() + gucci.buy("옷"));
         } else {
-            System.out.println("맘에 드는게 없어요~");
+            System.out.println("맘에 드는게 없어요~");    // 비어있을 때 나오는 문구
         }
     }
 }
